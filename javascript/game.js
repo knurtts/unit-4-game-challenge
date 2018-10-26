194394e92f6439b99faa7588268f8f34702ee8c9
Robert Anderson <robertwinstonanderson@gmail.com>
2018-10-26 00:04:21 -0600

    add some necessary divs to html, add pseudocode to game.js

---
+++ javascript/game.js
@@ -0,0 +1,33 @@
+//Make objects of each character with attack-power, HP, counter-attack-power
+
+//write function for when a character is clicked
+
+    //Player character will move to the "#arena-left" div.
+
+    //Change text in "#instructions" div to say "Select an opponent"
+
+//write a function for when an opponent is clicked
+
+    //Opponent moves to the "#arena-right" div
+
+    //Change instructions text to say, "Click 'Fight!' to begin battle"
+
+
+//Button click event
+    
+    //opponent health - champion attack
+    //update opponent health display
+    //update "#game-text" with champion attacked with X damage and opponent defended with Y damage
+
+    //champion attack += champion attack
+    //update champion attack display
+
+    //champion health - opponent attack
+    //update champion health display
+
+    //update "#game-text" with champion attacked with X damage and opponent defended with Y damage
+
+    //if opponent health = 0, change "#instructions" div to say "Choose another opponent"
+        //move opponent to graveyard
+    //if champion health = 0, alert "You have died"
+        //restart game
