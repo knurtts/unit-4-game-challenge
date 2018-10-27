215015e29a7aa41b9aef6b8d8afeb45f075af4db
Robert Anderson <robertwinstonanderson@gmail.com>
2018-10-26 18:01:33 -0600

    Add variables and start some functions. Add images to asset files

--- javascript/game.js
+++ javascript/game.js
@@ -1,17 +1,60 @@
 //Make objects of each character with attack-power, HP, counter-attack-power
+var luke = {
+    name: "Luke Skywalker",
+    attack: 10,
+    counter: 15,
+    health: 150,
+};
+
+var maul = {
+    name: "Darth Maul",
+    attack: 12,
+    counter: 17,
+    health: 170,
+};
+
+var kenobi = {
+    name: "Obi-wan Kenobi",
+    attack: 11,
+    counter: 16,
+    health: 160,
+}
+
+var vader = {
+    name: "Darth Vader",
+    attack: 15,
+    counter: 15,
+    health: 150,
+}
+
+var han = {
+    name: "Han Solo",
+    attack: 9,
+    counter: 12,
+    health: 120,
+}
+
+var battleStart = false;
+
+
 
 //write function for when a character is clicked
+selectCharacter = function() {
 
+    $(this).detatch().append("#arena-left");
     //Player character will move to the "#arena-left" div.
 
     //Change text in "#instructions" div to say "Select an opponent"
 
+}
+
+selectOpponent = function() {
 //write a function for when an opponent is clicked
 
     //Opponent moves to the "#arena-right" div
 
     //Change instructions text to say, "Click 'Fight!' to begin battle"
-
+}
 
 //Button click event
     
